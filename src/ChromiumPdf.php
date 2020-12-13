e7c1ba463c2968f975e081a5a9d3d79a5908357f
Benjamin Beganović <k1pstabug@gmail.com>
2020-12-13 22:40:33 +0100

    initial commit

---
+++ src/ChromiumPdf.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Beganovich\ChromiumPdf;
+
+class ChromiumPdf
+{
+    // ..
+}
---
+++ tests/ChromiumPdfTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\ChromiumPdf;
+
+use PHPUnit\Framework\TestCase;
+
+class ChromiumPdfTest extends TestCase
+{
+    /** @tests */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
